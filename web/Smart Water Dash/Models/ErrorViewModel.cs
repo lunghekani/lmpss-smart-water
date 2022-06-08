524493035683fb9407110b326e164bb8b3d61e62
lunghekani <lunghivlanga@gmail.com>
2022-06-08 12:44:16 +0200

    removed web

--- web/Smart Water Dash/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Smart_Water_Dash.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
