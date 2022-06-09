0b51b5fa7fd80c00a79d4068fbb422f6448de53d
Shakeel Dollie <shakzz23@gmail.com>
2022-06-09 11:01:07 +0200

    - User Model, Controller and View

---
+++ web/Smart Water Dash/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Smart_Water_Dash.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
